2106f4ee8388ed4cd49e45c98dce065148cb0db6
Igor Kaseko <igorksk@gmail.com>
2025-07-12 10:29:06 +0300

    init commit

---
+++ book_chapter1/task1_uniqueSymbols.js
@@ -0,0 +1,40 @@
+function uniqueWithoutDataStructures(str) {
+    for (let i = 0; i < str.length; i++) {
+      for (let j = i + 1; j < str.length; j++) {
+        if (str[i] === str[j]) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+  
+  function uniqueUsingDictionary(str) {
+    const dict = {};
+    for (const ch of str) {
+      if (dict[ch]) {
+        return false;
+      }
+      dict[ch] = true;
+    }
+    return true;
+  }
+  
+  function task1() {
+    const str = document.getElementById("inputString").value;
+    let result = "";
+  
+    if (uniqueWithoutDataStructures(str)) {
+      result += "Symbols unique by first check\n";
+    } else {
+      result += "Symbols not unique by first check\n";
+    }
+  
+    if (uniqueUsingDictionary(str)) {
+      result += "Symbols unique by second check\n";
+    } else {
+      result += "Symbols not unique by second check\n";
+    }
+  
+    document.getElementById("output").textContent = result;
+  }
