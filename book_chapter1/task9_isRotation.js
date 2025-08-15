04bfd124b7f521b0154d54eab241d97d10336960
Igor Kaseko <igorksk@gmail.com>
2025-08-15 10:30:45 +0300

    is rotation task

---
+++ book_chapter1/task9_isRotation.js
@@ -0,0 +1,22 @@
+function isRotation(str1, str2) {
+    if (str1.length !== str2.length) {
+        return false;
+    }
+
+    const concatenated = str1 + str1;
+    return concatenated.includes(str2);
+}
+
+function task9() {
+    const str1 = document.getElementById("inputString1").value;
+    const str2 = document.getElementById("inputString2").value;
+    let result = "";
+
+    if (isRotation(str1, str2)) {
+        result += "Strings are rotations of each other\n";
+    } else {
+        result += "Strings are not rotations of each other\n";
+    }
+
+    document.getElementById("output").textContent = result;
+}
